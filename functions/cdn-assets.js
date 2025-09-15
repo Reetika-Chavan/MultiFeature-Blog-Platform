c8c711db601eb07781bc55a17737ef2e898f03b2
Reetika-Chavan <reetika.chavan@contentstack.com>
2025-09-15 16:35:31 +0530

    cdnassets

--- functions/cdn-assets.js
+++ functions/cdn-assets.js
@@ -1,32 +1,42 @@
-export default async function handler(req, res) {
+// /functions/cdn-assets.js
+
+export default async function onRequest(context) {
   try {
-    const { path } = req.query; 
+    const reqUrl = new URL(context.request.url);
+    const requestedPath = reqUrl.pathname.replace(/^\/cdn-assets\/?/, "");
 
-    const assetBaseUrl = `https://${process.env.ASSET_BASE_HOST}/v3/assets/bltb27c897eae5ed3fb`;
+    const assetBaseUrl = `https://${context.env.ASSET_BASE_HOST}/v3/assets/bltb27c897eae5ed3fb`;
 
+    // Friendly map
     const assetMap = {
       "blog.png": "blt940544a43af4e6be/blog.png",
     };
 
-    const mappedPath = assetMap[path] || path;
+    const mappedPath = assetMap[requestedPath] || requestedPath;
+    const finalUrl = `${assetBaseUrl}/${mappedPath}${reqUrl.search}`;
 
-    const url = `${assetBaseUrl}/${mappedPath}`;
+    const upstreamRes = await fetch(finalUrl);
 
-    const searchParams = new URLSearchParams(req.query);
-    const finalUrl = `${url}?${searchParams.toString()}`;
-
-    const response = await fetch(finalUrl);
-
-    if (!response.ok) {
-      res.status(response.status).send("Error fetching asset");
-      return;
+    if (!upstreamRes.ok) {
+      return new Response(`Error fetching asset`, {
+        status: upstreamRes.status,
+      });
     }
 
-    const buffer = await response.arrayBuffer();
-    res.setHeader("Content-Type", response.headers.get("content-type"));
-    res.send(Buffer.from(buffer));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    const headers = new Headers();
+    headers.set(
+      "Content-Type",
+      upstreamRes.headers.get("content-type") || "application/octet-stream"
+    );
+    headers.set(
+      "Cache-Control",
+      upstreamRes.headers.get("cache-control") ||
+        "public, max-age=31536000, immutable"
+    );
+
+    return new Response(upstreamRes.body, { status: 200, headers });
+  } catch (err) {
+    console.error("cdn-assets error:", err);
+    return new Response("Internal Server Error", { status: 500 });
   }
 }
